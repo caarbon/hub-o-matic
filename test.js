137c12a5851b3c520b9e57d682e648d6a6ec5f87
tmarshall <timothyjmarshall@gmail.com>
2015-04-06 21:25:37 -0700

    pagination working

--- test.js
+++ test.js
@@ -1,5 +1,16 @@
 var x = require('./');
-console.log(process.env.GITHUB_TOKEN);
 var y = x(process.env.GITHUB_TOKEN);
 
-console.log('' + y);
+y.events(function(err, body, paging) {
+  console.log(err);
+  console.log(body);
+  console.log(paging);
+
+  setTimeout(function() {
+    paging.next(function(err, body) {
+      console.log(err);
+      console.log(body);
+      console.log(paging);
+    });
+  }, 3000);
+});
